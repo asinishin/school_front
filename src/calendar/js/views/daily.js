fa3310e3220b4df48ae6231c7e22ea23059b7f50
Maricel Sinishin <msinishin@si4b.com>
2013-10-21 14:49:01 -0700

    Added parts

--- src/calendar/js/views/daily.js
+++ src/calendar/js/views/daily.js
@@ -3,30 +3,63 @@
 define([
 	'backbone',
 	'underscore',
-	'text!templates/daily2.html'
+	'text!templates/daily1.html',
+	'text!templates/daily2.html',
+	'text!templates/daily3.html'
 ], function(
 	Backbone,
 	_,
-	dailyTemplate
+	dailyTemplate1,
+	dailyTemplate2,
+	dailyTemplate3
 ){  var DailyView = Backbone.View.extend({
 
+    id: "main",
 	events: {
+		'click #leftBtn':   'goLeft',
+		'click #rightBtn':  'goRight'
 	},
 
 	initialize: function(options) {
 		this.date = options.date;
+		this.part = options.part;
 	},
 	
-	load: function() {
-		return this;
-	},
-			
 	render: function() {
-		var template = _.template(dailyTemplate);
+		var template;
+		if (this.part === '1') {
+			template = _.template(dailyTemplate1);
+		} else if (this.part === '2') {
+			template = _.template(dailyTemplate2);
+		} else {
+			template = _.template(dailyTemplate3);
+		}
 		this.$el.html(template({ date: this.date }));
 
 	    return this;
 	},
+	
+	goLeft: function(event) {
+		event.preventDefault();
+		var p = parseInt(this.part);
+		if (p > 1) {
+			this.goTo(p - 1);
+		}
+	},
+	
+	goRight: function(event) {
+		event.preventDefault();
+		var p = parseInt(this.part);
+		if (p < 3) {
+			this.goTo(p + 1);
+		}
+	},
+	
+	goTo: function(part) {
+		var h = location.hash;
+		h = h.substring(1, h.length - 1) + part;
+		window.app.router.navigate(h, {trigger: true});
+	}
 	
 });
 
